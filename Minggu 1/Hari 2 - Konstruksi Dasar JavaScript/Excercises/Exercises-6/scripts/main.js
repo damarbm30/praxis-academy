2f8521c0b5ae52cb6ef6e50d037d8323df319fb1
Damar Buana Murti <damarbm30@gmail.com>
2022-08-18 11:30:43 +0700

    update exercise 1-5 hari 2 minggu 1

---
+++ Minggu 1/Hari 2 - Konstruksi Dasar JavaScript/Excercises/Exercises-6/scripts/main.js
@@ -0,0 +1,27 @@
+var students = [
+  ["David", 80],
+  ["Vinoth", 77],
+  ["Divya", 88],
+  ["Ishitha", 95],
+  ["Thomas", 68],
+];
+
+var totalMarks = 0;
+
+for (let i = 0; i < students.length; i++) {
+  totalMarks += students[i][1];
+}
+
+var avg = totalMarks / students.length;
+
+if (avg < 60) {
+  console.log("Grade: F");
+} else if (avg < 70) {
+  console.log("Grade: D");
+} else if (avg < 80) {
+  console.log("Grade: C");
+} else if (avg < 90) {
+  console.log("Grade: B");
+} else if (avg < 100) {
+  console.log("Grade: A");
+}
