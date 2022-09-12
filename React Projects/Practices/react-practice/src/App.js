1d16f8ae3a0a6507c69f483c466025607b53a264
Damar Buana Murti <damarbm30@gmail.com>
2022-09-12 09:16:54 +0700

    add fetch data practice project

--- React Projects/Practices/react-practice/src/App.js
+++ React Projects/Practices/react-practice/src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
-import FetchData from "./fetch-data";
-import FilterArray from "./filter-array/Filter";
-import DataBinding from "./simple-practice/data-binding";
-import DisableSubmit from "./simple-practice/disable-submit/DisableSubmit";
-import ReactChildren from "./simple-practice/react-children";
-import ShowHide from "./simple-practice/show-hide";
-import Parent from "./simple-practice/update-parent-state/Parent";
-import Slider from "./slider";
+import Axios from "./axios";
 
 function App() {
   return (
     <>
-      {/* <FilterArray /> */}
-      {/* <FetchData /> */}
-      <Slider />
-      {/* <ReactChildren /> */}
+      <Axios />
     </>
   );
 }
