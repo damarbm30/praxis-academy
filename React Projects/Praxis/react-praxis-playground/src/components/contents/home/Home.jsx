b40da29862ddca4eeb68f65fadcc6ba17b59a226
Damar Buana Murti <damarbm30@gmail.com>
2022-09-21 08:48:15 +0700

    update log out service in home section

--- React Projects/Praxis/react-praxis-playground/src/components/contents/home/Home.jsx
+++ React Projects/Praxis/react-praxis-playground/src/components/contents/home/Home.jsx
@@ -1,17 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import { userLogout } from "../../../_services";
 import "./home.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem("user");
-    navigate("/login");
-  };
-
   return (
     <div className="home-container">
-      <button className="logout-button" onClick={handleLogout}>
+      <button className="logout-button" onClick={() => userLogout(navigate)}>
         Logout
       </button>
     </div>
