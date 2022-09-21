ef8ae22ffc5b30f3298f67bce2eb2e6920813ae9
Damar Buana Murti <damarbm30@gmail.com>
2022-09-21 08:48:44 +0700

    update authService for login, register and log out

--- React Projects/Praxis/react-praxis-playground/src/_services/authService.js
+++ React Projects/Praxis/react-praxis-playground/src/_services/authService.js
@@ -4,11 +4,39 @@ const BASE_PATH = "https://nodejs-backend-api-playground.herokuapp.com";
 
 export const userRegistration = async (payload, navigate) => {
   try {
+    // eslint-disable-next-line
     const results = await axios.post(`${BASE_PATH}/auth/user/registration`, payload);
-    localStorage.setItem("userName", results.data.data.userName);
-    console.log(results.data.data.userName);
-    navigate("/home");
+    navigate("/login");
   } catch (error) {
     console.log(error);
   }
+};
+
+export const userLogin = async (payload, navigate) => {
+  try {
+    const results = await axios.post(`${BASE_PATH}/auth/user/login`, payload);
+    const token = await axios.get(`${BASE_PATH}/auth/generate/accessToken`, {
+      headers: {
+        Authorization: `Bearer ${results.data.refreshToken}`,
+      },
+    });
+
+    const userCredentials = {
+      ...results.data,
+      accessToken: token.data,
+    };
+
+    console.log(token.data);
+    console.log(results.data.refreshToken);
+
+    localStorage.setItem("userCredentials", JSON.stringify(userCredentials));
+    navigate("/home");
+  } catch (error) {
+    console.log(error.response.data);
+  }
+};
+
+export const userLogout = (navigate) => {
+  localStorage.removeItem("userCredentials");
+  navigate("/login");
 };
