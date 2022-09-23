0fe94b9f5cc4167fe5996144689d63befba313af
Damar Buana Murti <damarbm30@gmail.com>
2022-09-23 09:10:52 +0700

    update auth service functionality

--- React Projects/Praxis/react-praxis-playground/src/_services/authService.js
+++ React Projects/Praxis/react-praxis-playground/src/_services/authService.js
@@ -2,18 +2,23 @@ import axios from "axios";
 
 const BASE_PATH = "https://nodejs-backend-api-playground.herokuapp.com";
 
-export const userRegistration = async (payload, navigate) => {
+export const userRegistration = async (payload, navigate, setIsSent, setIsLoading, setShowNotification) => {
   try {
+    setShowNotification(true);
     // eslint-disable-next-line
+    setIsSent(true);
+    setIsLoading(true);
     const results = await axios.post(`${BASE_PATH}/auth/user/registration`, payload);
     navigate("/login");
+    setIsLoading(false);
   } catch (error) {
     console.log(error);
   }
 };
 
-export const userLogin = async (payload, navigate) => {
+export const userLogin = async (payload, navigate, setIsLoading, setErrMsg, setShowNotification) => {
   try {
+    setIsLoading(true);
     const results = await axios.post(`${BASE_PATH}/auth/user/login`, payload);
     const token = await axios.get(`${BASE_PATH}/auth/generate/accessToken`, {
       headers: {
@@ -26,13 +31,12 @@ export const userLogin = async (payload, navigate) => {
       accessToken: token.data,
     };
 
-    console.log(token.data);
-    console.log(results.data.refreshToken);
-
     localStorage.setItem("userCredentials", JSON.stringify(userCredentials));
     navigate("/home");
+    setIsLoading(false);
   } catch (error) {
-    console.log(error.response.data);
+    setShowNotification(true);
+    setErrMsg(error.response.data.message);
   }
 };
 
