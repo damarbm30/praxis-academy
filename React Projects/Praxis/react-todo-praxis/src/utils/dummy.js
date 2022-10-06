8e4e5435ecfb39cd18738bba7cafdc25155cab75
Damar Buana Murti <damarbm30@gmail.com>
2022-10-06 13:01:07 +0700

    add dummy data

---
+++ React Projects/Praxis/react-todo-praxis/src/utils/dummy.js
@@ -0,0 +1,6 @@
+export const todoList = [
+  { id: 1, title: "First todo", description: "Do it first" },
+  { id: 2, title: "Second todo", description: "Do it second" },
+  { id: 3, title: "Third todo", description: "Do it third" },
+  { id: 4, title: "Fourth todo", description: "Do it fourth" },
+];
