cbc34dedc8c58788f5afddb4a00e671ee81e070d
Damar Buana Murti <damarbm30@gmail.com>
2022-10-12 09:59:03 +0700

    update services for CRUD

--- React Projects/Praxis/react-todo-praxis/src/services/baseUrl.js
+++ React Projects/Praxis/react-todo-praxis/src/services/baseUrl.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "https://dummyjson.com" });
+// export default axios.create({ baseURL: "https://dummyjson.com" });
+// export default axios.create({ baseURL: "http://192.168.43.24:8080" });
+export default axios.create({ baseURL: "http://localhost:3500" });
--- React Projects/Praxis/react-todo-praxis/src/services/services.js
+++ React Projects/Praxis/react-todo-praxis/src/services/services.js
@@ -1,46 +1,93 @@
-import { todoList } from "../utils/dummy";
 import todoApi from "./baseUrl";
 
-export const createTodo = async (newTask, setTodos) => {
+export const createTodo = async (newTask, todos, setTodos) => {
   try {
-    const response = await todoApi.post("/todos", newTask);
-    setTodos((prevState) => [...prevState, response.data]);
+    const response = await todoApi.post("/create", newTask, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // setTodos((prevState) => [...prevState, response.data]);
+    setTodos((prevState) => [...prevState, newTask]);
   } catch (error) {
-    console.log(error.response.data.message);
+    console.log(error.message);
   }
 };
 
-export const getTodos = async (todos, setTodos) => {
+export const getTodos = async (setTodos) => {
   try {
-    const results = await todoApi.get("/todos");
-    setTodos(todoList);
-    console.log("Results: ", results.data.todos);
-    console.log("Todos: ", todos);
+    const results = await todoApi.get("/create");
+    if (results && results.data) {
+      setTodos(results.data);
+      localStorage.setItem("Todos", JSON.stringify(results.data));
+    }
   } catch (error) {
-    console.log(error.response.data.message);
+    console.log(error);
   }
 };
 
 export const updateTodo = async (editTask, setTodos, handleFinishEdit) => {
   try {
-    const response = await todoApi.put(`/todos/${editTask.id}`, editTask);
+    const response = await todoApi.put(
+      `/create/${editTask.id}`,
+      { title: editTask.title, description: editTask.description },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
     // without map method, the old todo will be included
     setTodos((prevStat) => {
       // map every todo in todos, check if the mapped todo id is the same with the one we are editing
       // if true, then replace it with the new todo, else, leave it be
-      return prevStat.map((task) => (task.id === editTask.id ? editTask : task));
+      return prevStat.map((task) => (task.id === editTask.id ? { ...response.data } : task));
     });
     handleFinishEdit();
   } catch (error) {
-    console.log(error.response.data.message);
+    console.log(error.response.data);
   }
 };
 
 export const deleteTodo = async (id) => {
   try {
-    const results = await todoApi.delete(`todos/${id}`);
+    const results = await todoApi.delete(`/create/${id}`);
     console.log(results.data);
+    console.log("Local storage deleteTodo: ", JSON.parse(localStorage.getItem("Todos")));
   } catch (error) {
     console.log(error.response.data.message);
   }
 };
+
+// export const addTodoDummy = async () => {
+//   try {
+//     const results = await todoApi.post(
+//       `/todos/add`,
+//       { todo: "Use DummyJSON in the project", completed: false, userId: 5 },
+//       {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//     console.log(results.data);
+//   } catch (error) {
+//     console.log(error.response.data.message);
+//   }
+// };
+
+// export const updateTodoDummy = async () => {
+//   try {
+//     const results = todoApi.put(
+//       "/todos/1",
+//       {
+//         completed: false,
+//       },
+//       { headers: { "Content-Type": "application/json" } }
+//     );
+//     console.log(results);
+//   } catch (error) {
+//     console.log(error.response.data.message);
+//   }
+// };
